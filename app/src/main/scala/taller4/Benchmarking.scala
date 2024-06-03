06ec66345e630a8b3a8fdd701511ff602856130a
JAlexis1 <ruizquicenojhonalexis@gmail.com>
2024-06-03 16:29:51 -0500

    actualizando paralelas

---
+++ app/src/main/scala/taller4/Benchmarking.scala
@@ -0,0 +1,54 @@
+package benchmarking
+
+import org.scalameter._
+import taller4.{Atomo, Div, Expo, Logaritmo, Newton, NewtonParalela, Numero, Prod, Resta, Suma}
+
+class Benchmarking {
+  def NewtonBenchmarking(): Unit = {
+
+
+    val objSecuencial= new Newton()
+    val objParalelizado = new NewtonParalela()
+    val e5 = Suma(Resta(Prod(Atomo('x'), Atomo('x')), Numero(1.0)), Prod(Numero(2.0), Atomo('x')))
+    val timeSecuencial = config(
+
+      Key.exec.minWarmupRuns := 10,
+      Key.exec.maxWarmupRuns := 10,
+      Key.exec.benchRuns := 20,
+      Key.verbose := true,
+    ) withWarmer new Warmer.Default measure {
+      objSecuencial.raizNewton(e5, Atomo('x'), 50000000.0, objSecuencial.buenaAprox)
+    }
+    val timeParalela = config(
+
+      Key.exec.minWarmupRuns := 10,
+      Key.exec.maxWarmupRuns := 10,
+      Key.exec.benchRuns := 20,
+      Key.verbose := true,
+    ) withWarmer new Warmer.Default measure {
+      objParalelizado.raizNewton(e5, Atomo('x'), 50000000.0, objParalelizado.buenaAprox)
+    }
+
+    val timeParalelaDer = config(
+
+      Key.exec.minWarmupRuns := 10,
+      Key.exec.maxWarmupRuns := 10,
+      Key.exec.benchRuns := 20,
+      Key.verbose := true,
+    ) withWarmer new Warmer.Default measure {
+      objParalelizado.derivar(Suma(Prod(Expo(Suma(Atomo('x'), Prod(Numero(3.0), Atomo('y'))), Numero(2.0)), Logaritmo(Resta(Atomo('z'), Div(Numero(4.0), Atomo('w'))))), Div(Resta(Expo(Atomo('v'), Numero(2.0)), Prod(Numero(5.0), Atomo('u'))), Suma(Logaritmo(Atomo('t')), Prod(Numero(6.0), Atomo('s'))))), Atomo('x'))
+    }
+    val timeSecuencialDer = config(
+      Key.exec.minWarmupRuns := 10,
+      Key.exec.maxWarmupRuns := 10,
+      Key.exec.benchRuns := 20,
+      Key.verbose := true,
+    ) withWarmer new Warmer.Default measure {
+      objSecuencial.derivar(Suma(Prod(Expo(Suma(Atomo('x'), Prod(Numero(3.0), Atomo('y'))), Numero(2.0)), Logaritmo(Resta(Atomo('z'), Div(Numero(4.0), Atomo('w'))))), Div(Resta(Expo(Atomo('v'), Numero(2.0)), Prod(Numero(5.0), Atomo('u'))), Suma(Logaritmo(Atomo('t')), Prod(Numero(6.0), Atomo('s'))))), Atomo('x'))
+    }
+    println(s"Tiempo de ejecución S: $timeSecuencial")
+    println(s"Tiempo de ejecución P: $timeParalela")
+    println(s"Tiempo de ejecución P der: $timeParalelaDer")
+    println(s"Tiempo de ejecución S  der: $timeSecuencialDer")
+  }
+}
--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -3,22 +3,23 @@
   * Autores: <Estudiantes>
   * Profesor: Carlos A Delgado
   */
+/**
+ * Taller 3 - Programación Funcional
+ * Autores: <Estudiantes>
+ * Profesor: Carlos A Delgado
+ */
 package taller4
 
+import benchmarking.Benchmarking
 import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
 
 object Taller4{
-
-  def saludo() = "Taller 4"
-
   def main(args: Array[String]): Unit = {
+    val benchmarking = new Benchmarking()
     val objnew = new Newton()
     val expr = Suma(Prod(Numero(2), Atomo('x')), Numero(3))
-    println(objnew.mostrar(expr))
-
-    val expr1 = Suma(Atomo('x'), Numero(2))
-    println(objnew.mostrar(expr1))
+    benchmarking.NewtonBenchmarking()
   }
- }
+}
